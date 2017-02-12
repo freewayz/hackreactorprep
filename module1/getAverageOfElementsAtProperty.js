6b639592d85fba68d9bce66809f46a7c2878e524
pitaside <fr33wayz@gmail.com>
2017-02-12 23:53:28 +0100

    implement get average element at property usign Array reduce

--- module1/getAverageOfElementsAtProperty.js
+++ module1/getAverageOfElementsAtProperty.js
@@ -1,3 +1,19 @@
 function getAverageOfElementsAtProperty(obj, key) {
   // your code here
+  if (obj.hasOwnProperty(key) 
+    && (obj[key] instanceof Array) && obj[key].length > 0) {
+      return obj[key].reduce(sum, 0) / obj[key].length
+  } 
+
+  return 0
 }
+
+function sum(a, b) {
+    return a + b
+}
+
+var obj = {
+  key: []
+};
+var output = getAverageOfElementsAtProperty(obj, 'key');
+console.log(output); // --> 2
