1eb05a513e0aa976915c7eed28f2c46395b030ce
pitaside <fr33wayz@gmail.com>
2017-02-16 13:31:01 +0100

    added logic to get smallest element at property

---
+++ module1/getLargestElementAtProperty.js
@@ -0,0 +1,28 @@
+function getLargestElementAtProperty(obj, key) {
+  // your code here
+
+  if (!obj.hasOwnProperty(key) || !(Array.isArray(obj[key]))){
+      return undefined;
+  }
+
+  var objArray = obj[key];
+  if (objArray.length <= 0) {
+      return undefined;
+  }
+
+  var result = 0;
+
+  for (var a = 0; a <= objArray.length -1; a++) {
+      if (objArray[a] <= result) {
+        result = objArray[a];
+      }
+  }
+
+  return result;
+}
+
+var obj = {
+  key: [2, 1, 5, 0, 9, 1]
+};
+var output = getSmallestElementAtProperty(obj, 'key');
+console.log(output); // --> 1
